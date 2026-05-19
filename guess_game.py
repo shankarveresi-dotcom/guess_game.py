c4c484b3a616c0e25919df5f2813ec70741173aa
shankar_ai <shankarveresi@gmail.com>
2026-05-19 10:28:18 +0530

    guess_game.py
    this is an intresting game designed by the python programming with basic simple logics

---
+++ guess_game.py
@@ -0,0 +1,11 @@
+import random
+
+secret_number = random.randint(1, 10)
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == secret_number:
+    print("🎉 Congratulations! You guessed correctly.")
+else:
+    print("❌ Wrong guess.")
+    print("The correct number was:", secret_number)
